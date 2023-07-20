150de1927ed0dd79f59a90490a7847b22bb42150
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2023-07-20 08:56:12 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Head from 'next/head'
+import Logo from '../components/logo'
+
+export default () => (
+    <div className="root">
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Next.js on Ultahost</title>
+        </Head>
+        <style jsx>{`
+      .root {
+        font-family: sans-serif;
+        line-height: 1.33rem;
+        margin-top: 8vh;
+      }
+      @media (min-width: 600px) {
+        .root {
+          margin-left: 21vw;
+          margin-right: 21vw;
+        }
+      }
+    `}</style>
+
+        <img src="/img/nextjs-logo.png" style={{ width: '10rem' }} alt="Next.js logo" />
+        <h1>Next.js on Ultahost</h1>
+
+        <p>Deploy <a href="https://nextjs.org/">Next.js</a> universal web apps on <a href="https://ultahost.com/">Ultahost VPS Server</a>.</p>
+
+        <p>This <strong>demo deployment on Ultahost</strong> is from the repo <a href="https://github.com/abdulazizahwan/ultahost-nextjs">abdulazizahwan/ultahost-nextjs</a>.</p>
+
+    </div>
+)
